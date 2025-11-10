3b57af531b746523a195d0a334727d20e001aa29
zhouchuanxiang <zhouchuanxiang-fx@bestpay.com.cn>
2025-11-10 16:26:14 +0800

    first commit

---
+++ output-toolkit-codegenerator/src/main/java/com/zhouchuanxiang/outputtoolkit/codegenerator/OutputToolkitCodegeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.zhouchuanxiang.outputtoolkit.codegenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutputToolkitCodegeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OutputToolkitCodegeneratorApplication.class, args);
+    }
+
+}
---
+++ output-toolkit-codegenerator/src/test/java/com/zhouchuanxiang/outputtoolkit/codegenerator/OutputToolkitCodegeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhouchuanxiang.outputtoolkit.codegenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutputToolkitCodegeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
